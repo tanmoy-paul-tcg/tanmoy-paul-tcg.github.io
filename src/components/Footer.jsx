e37ae68cf2b0c2d8841afe22f490469107e6d66f
Pritish Joshi <rjpritish@gmail.com>
2024-10-14 18:37:58 +0530

    Add data in footer component
    Signed-off-by: Pritish Joshi <rjpritish@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faReact, faBootstrap} from "@fortawesome/free-brands-svg-icons";
+import tcg from "../images/tcg.svg";
 
 function Footer() {
   return (
     <footer className="footer">
         <Container>
-            <div>
+            <Row>
+                <Col sm={4} className='footer-cols'>
+                    <a href="https:www.tcgcrest.org"><Image src={tcg} className='footer-img'/></a>
+                </Col>
+                <Col sm={4} className='footer-cols'>
+                    <span className='sec'>Address</span>
+                    <br/>
+                    Office:
+                    <br/>
+                    First Floor, Tower 1, Bengal Eco Intelligent Park (Techna), 
+                    Block EM, Plot No 3, Sector V, Salt lake, 
+                    Kolkata 700091, West Bengal, INDIA
+                    <br/>
+                    <br/>
+                    Laboratory:
+                    <br/>
+                    Third Floor, Delta Building, Bengal Intelligent Park, 
+                    Street No. 18, Block EP, Sector V, Salt lake, 
+                    Kolkata 700091, West Bengal, INDIA
+                </Col>
+                <Col sm={4} className='footer-cols'>
+                    <span className='sec'>Contact</span>
+                    <br/>
+                    E-mail:
+                    <br/>
+                    <a href="mailto:someone@example.com" target="_top">tanmoy.paul@tcgcrest.org</a>
+                    <br/>
+                    <a href="mailto:someone@example.com" target="_top">tanmoy.paul@tcgcrest.org</a>
+                </Col>
+            </Row>
+            <hr className="my-4" />
+            <Row>
                 <p><span className='sec'>&copy;</span> 2024 Pritish Joshi
                 <br/>
                 <br/>Written in <FontAwesomeIcon icon={faReact} /> & <FontAwesomeIcon icon={faBootstrap} /></p>
-            </div>
+            </Row>
             <style>
                 {`
                 .footer {
@@ -22,8 +54,29 @@ function Footer() {
                     bottom: 0;
                 }
 
-                .sec {
+                .footer-img {
+                    width: 80%;
+                }
+
+                .footer-cols {
+                    text-align: justify;
+                    padding-left: 20px;
+                    padding-right: 20px;
+                }
+
+                .footer .sec {
                     color: var(--secondary-color);
+                    font-weight: bold;
+                    font-size: 150%;
+                }
+
+                .footer a {
+                    color: var(--secondary-color);
+                }
+
+                .footer a:hover {
+                    cusror: pointer;
+                    font-size: 110%;
                 }
 
                 .fa-react {
