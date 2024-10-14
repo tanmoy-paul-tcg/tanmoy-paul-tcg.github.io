838612a197a24c762b6cb2c1e2ef761d3269cb4d
Pritish Joshi <rjpritish@gmail.com>
2024-10-14 23:19:05 +0530

    Add description in homepage
    Signed-off-by: Pritish Joshi <rjpritish@gmail.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Rimage from "./Rimage";
 import Bio from "./Bio";
 import Buttons from "./Buttons";
+import Typewriter from "typewriter-effect";
 
 function Home() {
   return (
@@ -18,20 +19,34 @@ function Home() {
       </Row>
       <hr className="my-4" />
       <Row>
-        <p>
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
-          <br/>   
+        <h3 className="type">
+        <Typewriter
+            options={{
+              cursor: "_",
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Welcome to ")
+                .pauseFor(1000)
+                .typeString("Materials Modelling Laboratory")
+                .start();
+            }}
+          />
+        </h3>
+        <p className="mt-3 catch">
+          <span className="sec">{'{  '}</span>
+          At the forefront of computational materials science, 
+          our lab pioneers the use of advanced computational techniques 
+          to design and discover cutting-edge materials for next-generation 
+          batteries and energy storage devices. 
+          Applying ab initio molecular dynamics, 
+          Density Functional Theory (DFT), and advanced methods 
+          including machine learning and neural networks, 
+          allows us to investigate material behavior at the atomic level.  
+          Our interdisciplinary team of computational physicists and chemists 
+          pushes the boundaries of materials innovation, leveraging AI/ML-driven models 
+          and high-performance simulations to accelerate the discovery of sustainable energy solutions. 
+          <span className="sec">{'  }'}</span>
         </p>
       </Row>
       <style>
@@ -39,6 +54,28 @@ function Home() {
        .col-lg-4 {
           justify-content: center;
         }
+
+        .type {
+          font-weight: bold;
+          text-align: center;
+          font-size: 28px;
+        }
+
+        .catch {
+          font-size: 18px;
+          font-weight: bold;
+        }
+
+        .catch .sec {
+          color: var(--secondary-color);
+          font-size: 22px;
+        }
+
+        @media (max-width: 767px) {
+            .catch {
+              text-align: justify;
+            }
+          }
         `}
       </style>
     </Container>
