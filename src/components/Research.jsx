2bd44cd2f0b2453a628ab3f8f0234b530fb67a1e
Pritish Joshi <rjpritish@gmail.com>
2024-10-15 11:48:21 +0530

    Added 'dummy' layout for Research page
    Signed-off-by: Pritish Joshi <rjpritish@gmail.com>

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -1,33 +1,163 @@
 import React from "react";
-import { Image , Container, Row } from "react-bootstrap";
+import { Image , Container, Row, Col, Card } from "react-bootstrap";
+import place from "../images/place.png";
 
 function Research() {
   return (
     <Container>
       <Row>
-      <h1>Not Found!</h1>
-      <h2>Research page is being developed</h2>
+        <Col lg={4}>
+          <h1>Research</h1>
+        </Col>
+      </Row>
+      <hr className="my-4" />
+      <Row>
+        <h2 className="mt-4">Battery Materials Modelling</h2>
+        <Col md={4}>
+          <Image src={place} className="res-img mb-2"/>
+        </Col>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                occaecat cupidatat non proident, sunt in culpa qui 
+                officia deserunt mollit anim id est laborum.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
       <Row>
-        <Image src='https://raw.githubusercontent.com/RISE-Crystals/RISE-Crystals.github.io/main/src/images/offline_1.svg' alt="Offline Image" className="off-img"/>
+        <h2 className="mt-4">Machine Learning & Neural Networks</h2>
+        <Col md={4}>
+          <Image src={place} className="res-img mb-2"/>
+        </Col>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                occaecat cupidatat non proident, sunt in culpa qui 
+                officia deserunt mollit anim id est laborum.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <h2 className="mt-4">Machine Learning Force Fields</h2>
+        <Col md={4}>
+          <Image src={place} className="res-img mb-2"/>
+        </Col>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                occaecat cupidatat non proident, sunt in culpa qui 
+                officia deserunt mollit anim id est laborum.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <h2 className="mt-4">Electronic Structure Studies</h2>
+        <Col md={4}>
+          <Image src={place} className="res-img mb-2"/>
+        </Col>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                occaecat cupidatat non proident, sunt in culpa qui 
+                officia deserunt mollit anim id est laborum.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <h2 className="mt-4">Reaction Dynamics Studies</h2>
+        <Col md={4}>
+          <Image src={place} className="res-img mb-2"/>
+        </Col>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                occaecat cupidatat non proident, sunt in culpa qui 
+                officia deserunt mollit anim id est laborum.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
       <style>
         {`
-        h1 {
-          color: var(--primary-color);
-          text-align: center;
-        }
+          h1 {
+            font-size: 50px;
+            font-weight: bold;
+          }
 
-        h2 {
-          color: var(--secondary-color);
-          text-align: center;
-        }
+          h2 {
+            color: var(--secondary-color);
+          }
 
-        .off-img {
-          width: 100%;
-          height: 100vh;
-          align-self: center;
-        }
+          .card {
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            transition: 0.3s;
+            border-radius: 10px;
+            height: 250px;
+          }
+          
+          .res-img {
+            border-radius: 10px;
+            width: 100%;
+            height: 250px;
+          }
+          
+          .card-text {
+            font-size: 18px;
+            line-height: 1.5;
+          }
+          
+          .card:hover {
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+          }
+          @media (max-width: 767px) {
+            .card {
+                height: var(--bs-card-height);
+            }
+          }
         `}
       </style>
     </Container>
