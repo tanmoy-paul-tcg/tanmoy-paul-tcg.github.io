0d3349603c49880ab20c559cc94091ab3f97f2ed
nishantaMishra <nishunm2000@gmail.com>
2024-10-15 19:02:02 +0530

    info added

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -80,14 +80,11 @@ function Research() {
           <Card>
             <Card.Body>
               <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit 
-                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-                occaecat cupidatat non proident, sunt in culpa qui 
-                officia deserunt mollit anim id est laborum.
+                Study of electronic structure like distribution of states and spins is key
+                in unerstanding nature of materials. For instance, all the materials predicted by our 
+                neural networs and other computational approaches is a valid battery 
+                material only if it has a band-gap. The analysis of electronic distribution is done 
+                by various DFT calculations. 
               </Card.Text>
             </Card.Body>
           </Card>
